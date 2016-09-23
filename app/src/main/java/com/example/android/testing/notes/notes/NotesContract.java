fc9d8248805fd210512daa93170391c1840b5e74
fervc49 <fernandoatlanta03@gmail.com>
2016-09-23 15:36:29 -0400

    Test Branch commnet

--- app/src/main/java/com/example/android/testing/notes/notes/NotesContract.java
+++ app/src/main/java/com/example/android/testing/notes/notes/NotesContract.java
@@ -27,7 +27,7 @@ import java.util.List;
  */
 public interface NotesContract {
 
-    //Interfaces to conect View with presenter New2 Branch
+    //Interfaces to conect View with presenter New2 BranchTest
     interface View {
 
         void setProgressIndicator(boolean active);
